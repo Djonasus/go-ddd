8e871fa15eddaddc2e6a4d37a7007049bcb59535
Djonasus <60808389+Djonasus@users.noreply.github.com>
2024-04-13 00:45:17 +0300

    comment update

--- internal/Infrastructure/User.go
+++ internal/Infrastructure/User.go
@@ -3,6 +3,7 @@ package infrastructure
 import (
 	entity "ddd/internal/Entity"
 	repository "ddd/internal/Repository"
+	"fmt"
 
 	"gorm.io/gorm"
 )
@@ -20,34 +21,39 @@ func NewUserInfrastructure(db *gorm.DB) repository.UserRepository {
 
 // Реализации интерфейса
 
-// Create implements user.UserRepository.
+// Create implements UserRepository.
 func (ui *UserInfrastructure) Create(user *entity.User) error {
-	modeledUser := UserEntityToModel(user)
-	res := ui.Db.Create(modeledUser)
+	// modeledUser := UserEntityToModel(user)
+	result := ui.Db.Limit(1).Where(UserModel{Login: user.Login}).Find(&UserModel{})
+	if result.RowsAffected != 0 {
+		return fmt.Errorf("user already exists")
+	}
+	res := ui.Db.Create(UserEntityToModel(user))
 	return res.Error
 }
 
-// Delete implements user.UserRepository.
+// Delete implements UserRepository.
 func (ui *UserInfrastructure) Delete(u *entity.User) error {
 	modeledUser := UserEntityToModel(u)
 	res := ui.Db.Delete(modeledUser)
 	return res.Error
 }
 
-// FindByLogin implements user.UserRepository.
+// FindByLogin implements UserRepository.
 func (ui *UserInfrastructure) FindByLogin(login string) (*entity.User, error) {
 	modeledUser := UserEntityToModel(&entity.User{Login: login})
 	res := ui.Db.Limit(1).Find(modeledUser)
 	return UserModelToEntity(modeledUser), res.Error
 }
 
-// Update implements user.UserRepository.
+// Update implements UserRepository.
 func (ui *UserInfrastructure) Update(u *entity.User) error {
 	modeledUser := UserEntityToModel(u)
 	res := ui.Db.Save(modeledUser)
 	return res.Error
 }
 
+// Преобразование модели в сущность
 func UserModelToEntity(um *UserModel) *entity.User {
 	return &entity.User{
 		Login:    um.Login,
@@ -57,6 +63,7 @@ func UserModelToEntity(um *UserModel) *entity.User {
 	}
 }
 
+// Преобразование сущности в модель
 func UserEntityToModel(u *entity.User) *UserModel {
 	return &UserModel{
 		Login:    u.Login,
